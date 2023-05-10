43ebb2f0fced83ffb5cd3a4f69cd4a4a5ab6374c
Tanuj Gupta <64638288+tanujgupta18@users.noreply.github.com>
2023-05-11 01:48:34 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.prettify())
+print(soup.title)
+
+text = soup.find_all(text = 'programming')
+print("List = ",text)
+size = len(text)
+print("Count = ",size)
